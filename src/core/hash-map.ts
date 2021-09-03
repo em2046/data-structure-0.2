1312c45b2598c044d3c5f9c4827b10240fc50eb7
jiangshengdev <jiangshengdev@outlook.com>
2021-09-03 21:13:15 +0800

    Hash map

--- src/core/hash-map.ts
+++ src/core/hash-map.ts
@@ -1,14 +1,12 @@
 import { LinkedList } from "./linked-list";
 import { Entry } from "./entry";
 import { hash } from "./hasher";
+import { equality } from "./equatable";
 
 // Copied from
-// https://github.com/openjdk/jdk/blob/1fb798d320c708dfcbc0bb157511a2937fafb9e6/src/java.base/share/classes/java/util/HashMap.java
+// https://github.com/openjdk/jdk/blob/1fb798d320c708dfcbc0bb157511a2937fafb9e6/src/java.base/share/classes/java/util/Hashtable.java
 
-const DEFAULT_INITIAL_CAPACITY = 1 << 4;
-const DEFAULT_LOAD_FACTOR = 0.75;
-const MAXIMUM_CAPACITY = 1 << 30;
-const ARRAY_MAX_LENGTH = 2 ** 32 - 1;
+const MAX_ARRAY_SIZE = 2 ** 32 - 1;
 
 export class HashMap<K, V> implements Iterable<[K, V]> {
   readonly #loadFactor: number;
@@ -16,54 +14,36 @@ export class HashMap<K, V> implements Iterable<[K, V]> {
   #size = 0;
   #table: LinkedList<Entry<K, unknown>>[] = [];
 
-  constructor(
-    initialCapacity = DEFAULT_INITIAL_CAPACITY,
-    loadFactor = DEFAULT_LOAD_FACTOR
-  ) {
-    if (initialCapacity > MAXIMUM_CAPACITY) {
-      initialCapacity = MAXIMUM_CAPACITY;
+  constructor(initialCapacity = 11, loadFactor = 0.75) {
+    if (initialCapacity < 0) {
+      throw new Error(`Illegal Capacity: ${initialCapacity}`);
     }
 
     if (loadFactor <= 0 || Number.isNaN(loadFactor)) {
-      throw new Error(`Illegal load factor: ${loadFactor}`);
+      throw new Error(`Illegal Load: ${initialCapacity}`);
+    }
+
+    if (initialCapacity === 0) {
+      initialCapacity = 1;
     }
 
     this.#loadFactor = loadFactor;
-    this.#threshold = (loadFactor * initialCapacity) | 0;
-
-    if (initialCapacity === MAXIMUM_CAPACITY) {
-      this.#threshold = ARRAY_MAX_LENGTH;
-    }
-
     this.#table = new Array(initialCapacity);
+    this.#threshold = Math.floor(
+      Math.min(initialCapacity * loadFactor, MAX_ARRAY_SIZE + 1)
+    );
   }
 
   get size(): number {
     return this.#size;
   }
 
-  get #capacity(): number {
-    return this.#table.length;
-  }
+  static from<K, V>(iterable: Iterable<[K, V]> = []): HashMap<K, V> {
+    const elements = [...iterable];
+    const map = new HashMap<K, V>(Math.max(2 * elements.length, 11), 0.75);
 
-  static from<K, V>(
-    iterable: Iterable<[K, V]> = [],
-    initialCapacity: number | undefined = undefined,
-    loadFactor = DEFAULT_LOAD_FACTOR
-  ): HashMap<K, V> {
-    let capacity = initialCapacity ?? DEFAULT_INITIAL_CAPACITY;
-
-    if (initialCapacity === undefined) {
-      const elements = [...iterable];
-
-      capacity = elements.length;
-    }
-
-    const map = new HashMap<K, V>(capacity, loadFactor);
-
-    for (const [key, value] of iterable) {
-      map.#set(key, value);
-      map.#size += 1;
+    for (const [key, value] of elements) {
+      map.set(key, value);
     }
 
     return map;
@@ -175,96 +155,93 @@ export class HashMap<K, V> implements Iterable<[K, V]> {
   }
 
   set(key: K, value: V): HashMap<K, V> {
-    if (this.#capacity === 0) {
-      this.#resize(DEFAULT_INITIAL_CAPACITY);
+    const table = this.#table;
+    const hashValue = hash(key);
+    const index = (hashValue & 0x7fffffff) % table.length;
+    const list = table[index];
+
+    if (list !== undefined) {
+      for (const entry of list) {
+        if (equality(entry.key, key)) {
+          entry.value = value;
+
+          return this;
+        }
+      }
     }
 
-    const raise = this.#set(key, value);
-
-    this.#size += raise;
-
-    if (raise !== 0 && this.#size > this.#threshold) {
-      this.#resize(this.#capacity << 1);
-    }
+    this.#addEntry(hashValue, key, value, index);
 
     return this;
   }
 
-  get(key: K): V | undefined {
-    const hashValue = hash(key);
-    const index = (hashValue & 0x7fffffff) % this.#capacity;
-    const list = this.#table[index];
+  #addEntry(hashValue: number, key: K, value: V, index: number): void {
+    let table = this.#table;
 
-    if (list === undefined) {
-      return undefined;
+    if (this.#size >= this.#threshold) {
+      this.rehash();
+
+      table = this.#table;
+      hashValue = hash(key);
+      index = index = (hashValue & 0x7fffffff) % table.length;
     }
-
-    const element = list.get(new Entry(key, {}));
-
-    if (element === undefined) {
-      return undefined;
-    }
-
-    return element.value as V;
-  }
-
-  delete(key: K): boolean {
-    const reduced = this.#delete(key);
-
-    if (reduced) {
-      this.#size -= 1;
-
-      if (this.#size < this.#threshold >> 2) {
-        this.#resize(this.#capacity >> 2);
-      }
-    }
-
-    return reduced;
-  }
-
-  clear(): void {
-    for (let i = 0; i < this.#capacity; i++) {
-      this.#table[i]?.clear();
-    }
-
-    this.#size = 0;
-    this.#resize(0);
-  }
-
-  #set(key: K, value: V): number {
-    const hashValue = hash(key);
-    const index = (hashValue & 0x7fffffff) % this.#capacity;
-    const table = this.#table;
 
     if (table[index] === undefined) {
       table[index] = new LinkedList<Entry<K, unknown>>();
     }
 
-    const list = table[index];
-    const size = list.size;
-
-    list.add(new Entry(key, value));
-
-    return list.size - size;
+    table[index].add(new Entry(key, value));
+    this.#size += 1;
   }
 
-  #delete(key: K): boolean {
-    const hashValue = hash(key);
-    const index = (hashValue & 0x7fffffff) % this.#capacity;
-    const list = this.#table[index];
+  rehash() {}
 
-    if (list === undefined) {
-      return false;
+  get(key: K): V | undefined {
+    const table = this.#table;
+    const hashValue = hash(key);
+    const index = (hashValue & 0x7fffffff) % table.length;
+    const list = table[index];
+
+    if (list !== undefined) {
+      for (const entry of list) {
+        if (equality(entry.key, key)) {
+          return entry.value as V;
+        }
+      }
     }
 
-    return list.delete(new Entry(key, {}));
+    return undefined;
   }
 
-  #resize(newCap: number): void {
-    const clone = HashMap.from([...this], newCap, this.#loadFactor);
+  delete(key: K): boolean {
+    const table = this.#table;
+    const hashValue = hash(key);
+    const index = (hashValue & 0x7fffffff) % table.length;
+    const list = table[index];
 
-    this.#table = clone.#table;
-    this.#size = clone.#size;
-    this.#threshold = clone.#threshold;
+    if (list !== undefined) {
+      const succeed = list.delete(new Entry<K, unknown>(key, {}));
+
+      if (succeed) {
+        this.#size -= 1;
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  clear(): void {
+    const table = this.#table;
+    const size = table.length;
+
+    for (let i = 0; i < size; i++) {
+      if (table[i] !== undefined) {
+        table[i].clear();
+      }
+    }
+
+    this.#size = 0;
   }
 }
